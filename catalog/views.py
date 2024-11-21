0d4c2a8e5e8b9976ae0b0d0cc16a6ee585ce32b7
Kafui Alordo <kalordo7@gmail.com>
2024-11-21 13:52:24 +0000

    Add basic functionality using the generic class-based and detail views to create pages and views

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
@@ -28,3 +29,20 @@ def index(request):
 
     # Render the HTML template index.html with the data ion the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+    # context_object_name = 'book_list'
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains="war")[:5]
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
