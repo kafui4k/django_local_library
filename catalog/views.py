b10f5550537014165912d5abfc796349ed468306
Kafui Alordo <kalordo7@gmail.com>
2024-11-24 12:40:28 +0000

    Add session to improve interations with anonymous users

--- catalog/views.py
+++ catalog/views.py
@@ -16,6 +16,11 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     # All the genres
     num_genres = Genre.objects.all().count()
 
@@ -25,6 +30,7 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres': num_genres,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data ion the context variable
