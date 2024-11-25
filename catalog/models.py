70eb2707c947fd0e00d2e3f917c123cf4154caf6
Kafui Alordo <kalordo7@gmail.com>
2024-11-25 14:56:45 +0000

    Enable user authentication and permissions on viewing contents

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.conf import settings
 from django.urls import reverse
-
+from datetime import date
 from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
 import uuid
@@ -85,12 +86,20 @@ class BookInstance(models.Model):
         help_text='Book availability'
     )
 
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+    
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back) 
     
 class Author(models.Model):
     """Model representing an author."""
